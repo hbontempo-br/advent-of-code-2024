3c37180b33be997e968bfb9ffe1dea04fa731c1b
Henrique Bontempo <me@hbontempo.dev>
2024-12-07 02:44:57 -0300

    Day 7

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -1,7 +1,62 @@
 defmodule AdventOfCode.Day07 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> parse_input
+    |> Stream.filter(&is_valid?/1)
+    |> Stream.map(fn {val, _} -> val end)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(args) do
+    args
+    |> parse_input
+    |> Stream.filter(&is_valid2?/1)
+    |> Stream.map(fn {val, _} -> val end)
+    |> Enum.sum()
+  end
+
+  defp is_valid?(test_case)
+  defp is_valid?({output, [head|tail]}), do: is_valid?(output, head, tail)
+  defp is_valid?(expected_output, current_output, other_values)
+  defp is_valid?(expected_output, current_output, []), do: expected_output == current_output
+  defp is_valid?(expected_output, current_output, [other_head| other_tail]) do
+    Kernel.or(
+        is_valid?(expected_output, current_output + other_head, other_tail),
+        is_valid?(expected_output, current_output * other_head, other_tail)
+    )
+  end
+
+  defp is_valid2?(test_case)
+  defp is_valid2?({output, [head|tail]}), do: is_valid2?(output, head, tail)
+  defp is_valid2?(expected_output, current_output, other_values)
+  defp is_valid2?(expected_output, current_output, []), do: expected_output == current_output
+  defp is_valid2?(expected_output, current_output, [other_head| other_tail]) do
+    is_valid2?(expected_output, current_output + other_head, other_tail) ||
+    is_valid2?(expected_output, current_output * other_head, other_tail) ||
+    is_valid2?(
+      expected_output,
+      String.to_integer(Integer.to_string(current_output) <> Integer.to_string(other_head)),
+      other_tail
+    )
+  end
+
+
+  defp parse_input(text) do
+    text
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(
+      fn line ->
+        [raw_left, raw_right] = String.split(line, ": ")
+        left = raw_left
+          |> String.trim()
+          |> String.to_integer()
+        right = raw_right
+          |> String.trim()
+          |> String.split(" ")
+          |> Enum.map(&String.to_integer/1)
+        { left, right }
+      end
+    )
   end
 end
--- test/advent_of_code/day_07_test.exs
+++ test/advent_of_code/day_07_test.exs
@@ -3,10 +3,20 @@ defmodule AdventOfCode.Day07Test do
 
   import AdventOfCode.Day07
 
-  @tag :skip
   test "part1" do
-    input = nil
-    expected_result = nil
+    input =
+      """
+      190: 10 19
+      3267: 81 40 27
+      83: 17 5
+      156: 15 6
+      7290: 6 8 6 15
+      161011: 16 10 13
+      192: 17 8 14
+      21037: 9 7 18 13
+      292: 11 6 16 20
+      """
+    expected_result = 3749
     result = part1(input)
 
     assert expected_result == result, "Expected: #{expected_result} / Got: #{result}"
@@ -14,8 +24,19 @@ defmodule AdventOfCode.Day07Test do
 
   @tag :skip
   test "part2" do
-    input = nil
-    expected_result = nil
+    input =
+      """
+      190: 10 19
+      3267: 81 40 27
+      83: 17 5
+      156: 15 6
+      7290: 6 8 6 15
+      161011: 16 10 13
+      192: 17 8 14
+      21037: 9 7 18 13
+      292: 11 6 16 20
+      """
+    expected_result = 11387
     result = part2(input)
 
     assert expected_result == result, "Expected: #{expected_result} / Got: #{result}"
