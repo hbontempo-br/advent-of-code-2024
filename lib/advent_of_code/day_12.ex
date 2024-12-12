38c866479849ceef538360b293764b85a6395753
Henrique Bontempo <me@hbontempo.dev>
2024-12-12 03:48:57 -0300

    Day 12

--- lib/advent_of_code/day_12.ex
+++ lib/advent_of_code/day_12.ex
@@ -1,7 +1,162 @@
 defmodule AdventOfCode.Day12 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> parse_input()
+    |> compute_regions()
+    |> Enum.map(&individual_price/1)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(args) do
+    args
+    |> parse_input()
+    |> compute_regions()
+    |> Enum.map(&bulk_price/1)
+    |> Enum.sum()
+  end
+
+  defp bulk_price(region) do
+    sides(region) * area(region)
+  end
+
+  defp individual_price(region) do
+    perimeter(region) * area(region)
+  end
+
+  # the number of sides is the same as number of corder
+  defp sides(region), do: corners(region)
+
+  defp corners(region) do
+    region
+    |> MapSet.to_list()
+    |> Enum.reduce(
+      0,
+      fn position, acc ->
+        [
+          {[:up,   :right], [             ]},
+          {[:up,   :left ], [             ]},
+          {[:down, :right], [             ]},
+          {[:down, :left ], [             ]},
+          {[:up_right    ], [:up,   :right]},
+          {[:up_left     ], [:up,   :left ]},
+          {[:down_right  ], [:down, :right]},
+          {[:down_left   ], [:down, :left ]},
+        ]
+        |> Stream.filter(
+          fn {not_members, members} ->
+            validation1 = not_members
+              |> Enum.all?(
+                fn dir ->
+                  next_position = next(position, dir)
+                  !MapSet.member?(region, next_position)
+                end
+              )
+            validation2 = members
+              |> Enum.all?(
+                fn dir ->
+                  next_position = next(position, dir)
+                  MapSet.member?(region, next_position)
+                end
+              )
+            validation1 && validation2
+          end
+        )
+        |> Enum.count()
+        |> Kernel.+(acc)
+      end
+    )
+  end
+
+  defp perimeter(region) do
+    region
+    |> MapSet.to_list()
+    |> Enum.reduce(
+      0,
+      fn position, acc ->
+        [:up, :down, :left, :right]
+        |> Stream.filter(
+          fn dir ->
+            next_position = next(position, dir)
+            !MapSet.member?(region, next_position)
+          end
+        )
+        |> Enum.count()
+        |> Kernel.+(acc)
+      end
+    )
+  end
+
+  defp area(region), do: MapSet.size(region)
+
+  defp compute_regions(map), do: compute_regions(map, [], all_positions(map))
+  defp compute_regions(map, regions, positions) do
+    case MapSet.size(positions) == 0 do
+      true -> regions
+      false ->
+        position = Enum.random(positions)
+        new_region = region(position, map)
+
+        new_regions = [new_region|regions]
+        new_positions = MapSet.difference(positions, new_region)
+
+        compute_regions(map, new_regions, new_positions)
+    end
+  end
+
+  defp region(position, map)
+  defp region(position, map), do: region(position, Map.get(map, position), map, MapSet.new())
+  defp region(position, value, map, region) do
+    case MapSet.member?(region, position) do
+      true -> region
+      false ->
+        case Map.get(map, position) == value do
+          false -> region
+          true ->
+            new_region = MapSet.put(region, position)
+
+            [:up, :down, :left, :right]
+            |> Enum.reduce(
+              new_region,
+              fn dir, acc ->
+                next_position = next(position, dir)
+                region(next_position, value, map, acc)
+              end
+            )
+        end
+    end
+  end
+
+
+
+  defp next(position, direction)
+  defp next({ x, y }, :up),         do: { x-1, y   }
+  defp next({ x, y }, :down),       do: { x+1, y   }
+  defp next({ x, y }, :left),       do: { x  , y-1 }
+  defp next({ x, y }, :right),      do: { x  , y+1 }
+  defp next({ x, y }, :up_right),   do: { x-1, y+1 }
+  defp next({ x, y }, :up_left),    do: { x-1, y-1 }
+  defp next({ x, y }, :down_right), do: { x+1, y+1 }
+  defp next({ x, y }, :down_left),  do: { x+1, y-1 }
+
+  defp all_positions(map), do: map |> Map.keys() |> MapSet.new()
+
+  defp parse_input(text) do
+    text
+    |> String.trim()
+    |> String.split("\n")
+    |> Stream.with_index()
+    |> Stream.flat_map(
+      fn {line, row} ->
+        line
+        |> String.trim()
+        |> String.codepoints()
+        |> Stream.with_index()
+        |> Enum.map(fn {val, column} -> {{row, column}, val} end)
+      end
+    )
+    |> Enum.reduce(
+      Map.new(),
+      fn {position, val}, acc -> Map.put(acc, position, val) end
+    )
   end
 end
--- test/advent_of_code/day_12_test.exs
+++ test/advent_of_code/day_12_test.exs
@@ -3,19 +3,133 @@ defmodule AdventOfCode.Day12Test do
 
   import AdventOfCode.Day12
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    expected_result = nil
+  test "part1 - example 1" do
+    input =
+      """
+      AAAA
+      BBCD
+      BBCC
+      EEEC
+      """
+    expected_result = 140
     result = part1(input)
 
     assert expected_result == result
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
-    expected_result = nil
+  test "part1 - example 2" do
+    input =
+      """
+      OOOOO
+      OXOXO
+      OOOOO
+      OXOXO
+      OOOOO
+      """
+    expected_result = 772
+    result = part1(input)
+
+    assert expected_result == result
+  end
+
+  test "part1 - example 3" do
+    input =
+      """
+      RRRRIICCFF
+      RRRRIICCCF
+      VVRRRCCFFF
+      VVRCCCJFFF
+      VVVVCJJCFE
+      VVIVCCJJEE
+      VVIIICJJEE
+      MIIIIIJJEE
+      MIIISIJEEE
+      MMMISSJEEE
+      """
+    expected_result = 1930
+    result = part1(input)
+
+    assert expected_result == result
+  end
+
+  test "part2 - example 1" do
+    input =
+      """
+      AAAA
+      BBCD
+      BBCC
+      EEEC
+      """
+    expected_result = 80
+    result = part2(input)
+
+    assert expected_result == result
+  end
+
+  test "part2 - example 2" do
+    input =
+      """
+      OOOOO
+      OXOXO
+      OOOOO
+      OXOXO
+      OOOOO
+      """
+    expected_result = 436
+    result = part2(input)
+
+    assert expected_result == result
+  end
+
+  test "part2 - example 3" do
+    input =
+      """
+      EEEEE
+      EXXXX
+      EEEEE
+      EXXXX
+      EEEEE
+      """
+    expected_result = 236
+    result = part2(input)
+
+    assert expected_result == result
+  end
+
+
+  test "part2 - example 4" do
+    input =
+      """
+      AAAAAA
+      AAABBA
+      AAABBA
+      ABBAAA
+      ABBAAA
+      AAAAAA
+      """
+    expected_result = 368
+    result = part2(input)
+
+    assert expected_result == result
+  end
+
+
+
+  test "part2 - example 5" do
+    input =
+      """
+      RRRRIICCFF
+      RRRRIICCCF
+      VVRRRCCFFF
+      VVRCCCJFFF
+      VVVVCJJCFE
+      VVIVCCJJEE
+      VVIIICJJEE
+      MIIIIIJJEE
+      MIIISIJEEE
+      MMMISSJEEE
+      """
+    expected_result = 1206
     result = part2(input)
 
     assert expected_result == result
