ef9f1676072fdedfd419cd7353bbbe592bfc9d2b
Henrique Bontempo <me@hbontempo.dev>
2024-12-16 23:53:07 -0300

    Day 16 - Part 2

--- lib/advent_of_code/day_16.ex
+++ lib/advent_of_code/day_16.ex
@@ -1,60 +1,136 @@
 defmodule AdventOfCode.Day16 do
   def part1(args) do
-    { start_position, end_position, map } = parse_input(args)
-    find_score({start_position, :right}, end_position, map)
+    {start_position, end_position, map} = parse_input(args)
+    {score, _visited} = find_score({start_position, :right}, end_position, map)
+    score
   end
 
-  def part2(_args) do
+  def part2(args) do
+    {start_position, end_position, map} = parse_input(args)
+    {_score, visited} = find_score({start_position, :right}, end_position, map)
+    paths = get_paths(visited, end_position, {start_position, :right})
+    seats = best_seats(paths)
+    Enum.count(seats)
+  end
+
+  defp best_seats(paths) do
+    paths |> Enum.reduce([], &(&1++&2)) |> Enum.uniq()
+  end
+
+  defp get_paths(visited, end_position, start_state) do
+    visited
+    |> Map.keys()
+    |> Enum.filter(&(elem(&1, 0) == end_position))
+    |> Enum.flat_map(&get_paths(&1, visited, start_state, [[]]))
+    |> Enum.map(fn path ->
+      path
+      |> Enum.map(&elem(&1, 0))
+      |> Enum.uniq()
+    end)
+    |> Enum.uniq()
+  end
+
+  defp get_paths(state, _visited, until_state, paths) when state == until_state, do: paths
+
+  defp get_paths(state, visited, until_state, paths) do
+    visited
+    |> Map.get(state)
+    |> elem(1)
+    |> Enum.flat_map(fn new_state ->
+      paths
+      |> Enum.flat_map(fn path ->
+        get_paths(new_state, visited, until_state, [[state | path]])
+      end)
+    end)
   end
 
   defp update_computations(state, score, estimates, visited, map) do
-    new_visited = MapSet.put(visited, state)
+    current_state_estimate = Map.get(estimates, state)
+    new_visited = Map.put(visited, state, current_state_estimate)
     initial_estimates = Map.delete(estimates, state)
 
-    new_estimates = [
-      { rotate(state, :counterclockwise), score + 1000 },
-      { rotate(state, :clockwise       ), score + 1000 },
-      { rotate(state, :inverse         ), score + 2000 },
-      { next(state)                     , score + 1    }
-    ]
-    |> Enum.filter(fn {new_state, _} -> !MapSet.member?(visited, new_state) end)
-    |> Enum.filter(fn {new_state, _} -> valid?(new_state, map) end)
-    |> Enum.reduce(
-      initial_estimates,
-      fn {new_state, new_score}, acc ->
-        Map.update(acc,new_state,new_score,&(min(&1,new_score)))
-      end
-    )
+    new_estimates =
+      [
+        {rotate(state, :counterclockwise), score + 1000},
+        {rotate(state, :clockwise), score + 1000},
+        {rotate(state, :inverse), score + 2000},
+        {next(state), score + 1}
+      ]
+      |> Enum.filter(fn {new_state, _} -> !Map.has_key?(new_visited, new_state) end)
+      |> Enum.filter(fn {new_state, _} -> valid?(new_state, map) end)
+      |> Enum.reduce(
+        initial_estimates,
+        fn {new_state, new_score}, acc ->
+          Map.update(
+            acc,
+            new_state,
+            {new_score, [state]},
+            fn
+              {current_score, _previous_states} = current_estimate
+              when new_score > current_score ->
+                current_estimate
 
-    { new_estimates, new_visited }
+              {current_score, _previous_states} = _current_estimate
+              when new_score < current_score ->
+                {new_score, [state]}
+
+              {current_score, previous_states} = _current_estimate
+              when new_score == current_score ->
+                {current_score, [state | previous_states]}
+            end
+          )
+        end
+      )
+
+    {new_estimates, new_visited}
   end
 
   defp next_state(estimates) do
     estimates
     |> Map.to_list()
-    |> Enum.min_by(&(elem(&1, 1)))
+    |> Enum.min_by(fn {_state, {score, _previous_states}} -> score end)
+    |> then(fn {state, {score, _previous_states}} -> {state, score} end)
   end
 
   defp find_score(initial_state, end_position, map) do
-    find_score(initial_state, 0, Map.new(), MapSet.new(), end_position, map)
+    find_score(initial_state, 0, Map.new([{initial_state, []}]), Map.new(), end_position, map)
   end
+
   defp find_score(state, score, estimates, visited, end_position, map)
+
   defp find_score(
-    {position, _direction}=_state,
-    score,
-    _estimates,
-    _visited,
-    end_position,
-    _map
-  ) when position==end_position, do: score
+         {position, _direction} = _state,
+         score,
+         estimates,
+         visited,
+         end_position,
+         _map
+       )
+       when position == end_position do
+    estimates
+    |> Map.to_list()
+    |> Enum.filter(fn {{estimate_position, _estimate_direction} = _estimate_state,
+                       {estimate_score, _previous_states}} ->
+      estimate_position == end_position && estimate_score == score
+    end)
+    |> Enum.reduce(
+      visited,
+      fn {estimate_state, estimate_state_estimate}, acc ->
+        Map.put(acc, estimate_state, estimate_state_estimate)
+      end
+    )
+    |> then(&{score, &1})
+  end
+
   defp find_score(state, score, estimates, visited, end_position, map) do
-    { new_estimates, new_visited } = update_computations(state, score, estimates, visited, map)
-    { new_state, new_score } = next_state(new_estimates)
+    {new_estimates, new_visited} = update_computations(state, score, estimates, visited, map)
+    {new_state, new_score} = next_state(new_estimates)
     find_score(new_state, new_score, new_estimates, new_visited, end_position, map)
   end
 
   defp valid?(state, map) do
-    {position, _}=state
+    {position, _} = state
+
     case Map.get(map, position) do
       :road -> true
       _ -> false
@@ -62,43 +138,46 @@ defmodule AdventOfCode.Day16 do
   end
 
   defp next(states)
-  defp next({{x, y}, :up   }), do: {{x-1, y  }, :up   }
-  defp next({{x, y}, :down }), do: {{x+1, y  }, :down }
-  defp next({{x, y}, :left }), do: {{x  , y-1}, :left }
-  defp next({{x, y}, :right}), do: {{x  , y+1}, :right}
+  defp next({{x, y}, :up}), do: {{x - 1, y}, :up}
+  defp next({{x, y}, :down}), do: {{x + 1, y}, :down}
+  defp next({{x, y}, :left}), do: {{x, y - 1}, :left}
+  defp next({{x, y}, :right}), do: {{x, y + 1}, :right}
 
   defp rotate(state, direction)
-  defp rotate({position, :up   }, :counterclockwise), do: {position, :left }
-  defp rotate({position, :left }, :counterclockwise), do: {position, :down }
-  defp rotate({position, :down }, :counterclockwise), do: {position, :right}
-  defp rotate({position, :right}, :counterclockwise), do: {position, :up   }
-  defp rotate({position, :up   }, :clockwise       ), do: {position, :right}
-  defp rotate({position, :right}, :clockwise       ), do: {position, :down }
-  defp rotate({position, :down }, :clockwise       ), do: {position, :left }
-  defp rotate({position, :left }, :clockwise       ), do: {position, :up   }
-  defp rotate({position, :up   }, :inverse         ), do: {position, :down }
-  defp rotate({position, :right}, :inverse         ), do: {position, :left }
-  defp rotate({position, :down }, :inverse         ), do: {position, :up   }
-  defp rotate({position, :left }, :inverse         ), do: {position, :right}
+  defp rotate({position, :up}, :counterclockwise), do: {position, :left}
+  defp rotate({position, :left}, :counterclockwise), do: {position, :down}
+  defp rotate({position, :down}, :counterclockwise), do: {position, :right}
+  defp rotate({position, :right}, :counterclockwise), do: {position, :up}
+  defp rotate({position, :up}, :clockwise), do: {position, :right}
+  defp rotate({position, :right}, :clockwise), do: {position, :down}
+  defp rotate({position, :down}, :clockwise), do: {position, :left}
+  defp rotate({position, :left}, :clockwise), do: {position, :up}
+  defp rotate({position, :up}, :inverse), do: {position, :down}
+  defp rotate({position, :right}, :inverse), do: {position, :left}
+  defp rotate({position, :down}, :inverse), do: {position, :up}
+  defp rotate({position, :left}, :inverse), do: {position, :right}
 
   defp parse_input(text) do
     map = parse_map(text)
 
-    start_position = map
+    start_position =
+      map
       |> Map.to_list()
-      |> Enum.find(&(elem(&1,1)==:start))
+      |> Enum.find(&(elem(&1, 1) == :start))
       |> elem(0)
 
-    end_position = map
+    end_position =
+      map
       |> Map.to_list()
-      |> Enum.find(&(elem(&1,1)==:end))
+      |> Enum.find(&(elem(&1, 1) == :end))
       |> elem(0)
 
-    new_map = map
+    new_map =
+      map
       |> Map.put(start_position, :road)
       |> Map.put(end_position, :road)
 
-    { start_position, end_position, new_map }
+    {start_position, end_position, new_map}
   end
 
   defp parse_map(text) do
@@ -106,19 +185,15 @@ defmodule AdventOfCode.Day16 do
     |> String.trim()
     |> String.split("\n")
     |> Stream.with_index()
-    |> Stream.flat_map(
-      fn {line, row} ->
-        line
-        |> String.trim()
-        |> String.codepoints()
-        |> Stream.with_index()
-        |> Enum.map(
-          fn {val, column} ->
-            {{row, column}, parse_object(val)}
-          end
-        )
-      end
-    )
+    |> Stream.flat_map(fn {line, row} ->
+      line
+      |> String.trim()
+      |> String.codepoints()
+      |> Stream.with_index()
+      |> Enum.map(fn {val, column} ->
+        {{row, column}, parse_object(val)}
+      end)
+    end)
     |> Map.new()
   end
 
--- test/advent_of_code/day_16_test.exs
+++ test/advent_of_code/day_16_test.exs
@@ -55,10 +55,26 @@ defmodule AdventOfCode.Day16Test do
     assert expected_result == result
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    expected_result = nil
+    input =
+      """
+      ###############
+      #.......#....E#
+      #.#.###.#.###.#
+      #.....#.#...#.#
+      #.###.#####.#.#
+      #.#.#.......#.#
+      #.#.#####.###.#
+      #...........#.#
+      ###.#.#####.#.#
+      #...#.....#.#.#
+      #.#.#.###.#.#.#
+      #.....#...#.#.#
+      #.###.#.#.#.#.#
+      #S..#.....#...#
+      ###############
+      """
+    expected_result = 45
     result = part2(input)
 
     assert expected_result == result
