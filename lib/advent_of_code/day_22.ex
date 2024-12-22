3f3ebb5cd3d098b2c246c6adb0aa63d929cc209c
Henrique Bontempo <me@hbontempo.dev>
2024-12-22 03:10:22 -0300

    Day 22 - Part 1

--- lib/advent_of_code/day_22.ex
+++ lib/advent_of_code/day_22.ex
@@ -1,7 +1,58 @@
 defmodule AdventOfCode.Day22 do
-  def part1(_args) do
+
+  def part1(args, times \\ 2000) do
+    args
+    |> parse()
+    |> Enum.map(&(secret_number(&1, times)))
+    |> Enum.sum()
   end
 
+
   def part2(_args) do
+  end
+
+  defp secret_number(current, times)
+  defp secret_number(current, 0), do: current
+  defp secret_number(current, times) do
+    next = next_secret_number(current)
+    secret_number(next, times-1)
+  end
+
+  defp next_secret_number(current) do
+    current
+    |> process1()
+    |> process2()
+    |> process3()
+  end
+
+  defp process1(secret_number) do
+    secret_number * 64
+    |> mix(secret_number)
+    |> prune()
+  end
+
+  defp process2(secret_number) do
+    secret_number / 32
+    |> Kernel.trunc()
+    |> mix(secret_number)
+    |> prune()
+  end
+
+  defp process3(secret_number) do
+    secret_number * 2048
+    |> mix(secret_number)
+    |> prune()
+  end
+
+  defp mix(val, secret_number), do: Bitwise.bxor(val, secret_number)
+
+  defp prune(val), do: rem(val, 16777216)
+
+  defp parse(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Stream.map(&String.trim/1)
+    |> Enum.map(&String.to_integer/1)
   end
 end
--- test/advent_of_code/day_22_test.exs
+++ test/advent_of_code/day_22_test.exs
@@ -3,10 +3,27 @@ defmodule AdventOfCode.Day22Test do
 
   import AdventOfCode.Day22
 
-  @tag :skip
+  test "execute" do
+    input =
+      """
+      123
+      """
+    times = 1
+    expected_result = 15887950
+    result = part1(input, times)
+
+    assert expected_result == result
+  end
+
   test "part1" do
-    input = nil
-    expected_result = nil
+    input =
+      """
+      1
+      10
+      100
+      2024
+      """
+    expected_result = 37327623
     result = part1(input)
 
     assert expected_result == result
