ac467c9a4f1e7f66cff87019c800edfad1cc574f
Henrique Bontempo <me@hbontempo.dev>
2024-12-23 03:25:53 -0300

    Day 23 - Part 1

--- lib/advent_of_code/day_23.ex
+++ lib/advent_of_code/day_23.ex
@@ -1,7 +1,68 @@
 defmodule AdventOfCode.Day23 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> parse()
+    |> find_networks()
+    |> Enum.count()
   end
 
   def part2(_args) do
+  end
+
+  defp find_networks(network_map) do
+    initial_networks = network_map
+      |> Map.to_list()
+      |> Enum.filter(&(String.starts_with?(elem(&1,0), "t")))
+      |> Enum.map(
+        fn {computer, connected_computers} ->
+          {MapSet.new([computer]), connected_computers}
+        end
+      )
+
+    initial_networks
+    |> grow_networks(network_map)
+    |> grow_networks(network_map)
+  end
+
+  defp grow_networks(networks, network_map) do
+    networks
+    |> Enum.flat_map(&(grow_network(&1,network_map)))
+    |> Enum.uniq()
+  end
+
+  defp grow_network(network, network_map)
+  defp grow_network({current_network, connected_computers}, network_map) do
+    connected_computers
+    |> Enum.to_list()
+    |> Enum.map(
+      fn connected_computer ->
+        new_network = MapSet.put(current_network, connected_computer)
+        connected_computer_connections = Map.get(network_map, connected_computer)
+        new_connected_computers = MapSet.intersection(connected_computers, connected_computer_connections)
+        {new_network, new_connected_computers}
+      end
+    )
+  end
+
+
+
+
+  defp parse(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Stream.map(&String.trim/1)
+    |> Stream.flat_map(
+      fn line ->
+        [computer1, computer2] = String.split(line, "-")
+        [{computer1, computer2}, {computer2, computer1}]
+      end
+    )
+    |> Enum.reduce(
+      Map.new(),
+      fn {computer1, computer2}, acc ->
+        Map.update(acc, computer1, MapSet.new([computer2]), &(MapSet.put(&1,computer2)))
+      end
+    )
   end
 end
--- test/advent_of_code/day_23_test.exs
+++ test/advent_of_code/day_23_test.exs
@@ -3,10 +3,43 @@ defmodule AdventOfCode.Day23Test do
 
   import AdventOfCode.Day23
 
-  @tag :skip
   test "part1" do
-    input = nil
-    expected_result = nil
+    input =
+      """
+      kh-tc
+      qp-kh
+      de-cg
+      ka-co
+      yn-aq
+      qp-ub
+      cg-tb
+      vc-aq
+      tb-ka
+      wh-tc
+      yn-cg
+      kh-ub
+      ta-co
+      de-co
+      tc-td
+      tb-wq
+      wh-td
+      ta-ka
+      td-qp
+      aq-cg
+      wq-ub
+      ub-vc
+      de-ta
+      wq-aq
+      wq-vc
+      wh-yn
+      ka-de
+      kh-ta
+      co-tc
+      wh-qp
+      tb-vc
+      td-yn
+      """
+    expected_result = 7
     result = part1(input)
 
     assert expected_result == result
